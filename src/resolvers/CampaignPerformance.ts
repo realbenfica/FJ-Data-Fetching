cb92d65cb2a00cfa45065cba09b20c246f5c1ba0
Mayron <mvanleenden@gmail.com>
2019-03-13 11:45:58 +0100

    Campaign performance resolver graphql

---
+++ src/resolvers/CampaignPerformance.ts
@@ -0,0 +1,88 @@
+import TrackedCampaignVideoAd from '../TrackedCampaignVideoAd/model'
+import Campaign from '../Campaign/model'
+import VideoAd from '../VideoAd/model'
+
+const CampaignPerformance = {
+  videoAdPerformance: async ({id}) => {
+
+    const positions:any = await TrackedCampaignVideoAd
+        .query(`
+        SELECT position 
+        FROM tracked_campaign_video_ads 
+        WHERE tracked_campaign_id = '${id}' 
+        GROUP BY position 
+        ORDER BY position ASC`)
+
+    const combinedVideoPerformance:IVideoAdPerformance[] = []
+
+    for(let { position } of positions) {
+
+      const trackedVideos: TrackedCampaignVideoAd[] = await TrackedCampaignVideoAd.find({trackedCampaignId: id, position})
+      
+      let unique_views:number = 0
+      let ctr:number = 0
+      let retention:number = 0
+      let cpv:number = 0
+      let spend: number = 0
+      
+      const videos:IVideoAd[] = []
+
+      for(let video of trackedVideos) {
+        const videoDetail = await VideoAd.findOne({ id: video.videoAdId })
+        const campaign:Campaign = await Campaign.findOne({ id: videoDetail.campaignId })
+
+        unique_views = unique_views + parseFloat(videoDetail.uniqueView)
+        ctr = ctr + parseFloat(videoDetail.ctr)
+        cpv = cpv + parseFloat(videoDetail.cpv)
+        retention = retention + parseFloat(videoDetail.retention)
+        spend = spend + parseFloat(videoDetail.spend)
+
+        videos.push({
+          id: videoDetail.id,
+          name: videoDetail.name,
+          cpv: videoDetail.cpv,
+          ctr: videoDetail.ctr,
+          unique_views: videoDetail.uniqueView,
+          retention: videoDetail.retention,
+          thumbnails: videoDetail.thumbnails,
+          spend: videoDetail.spend,
+          platform: campaign.platform.toString()
+        })
+      }
+
+      // Calculating the average
+      ctr = ctr / trackedVideos.length
+      cpv = cpv / trackedVideos.length
+      retention = retention / trackedVideos.length
+      
+      combinedVideoPerformance.push({unique_views, ctr, cpv, retention, spend, videos})
+    }
+
+    return combinedVideoPerformance
+  }
+}
+
+interface IVideoAdPerformance {
+  unique_views: Number
+  retention: Number
+  cpv: Number
+  ctr: Number
+  spend: Number
+  videos: IVideoAd[]
+}
+
+interface IVideoAd {
+  id: string
+  name: string
+  cpv: string
+  ctr: string
+  unique_views:string
+  spend: string
+  retention: string
+  thumbnails: string
+  platform: string
+}
+
+export {
+  CampaignPerformance
+}
