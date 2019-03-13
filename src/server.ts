e723fad2f57fad8a632795826ba746e97b8b487a
Mayron <mvanleenden@gmail.com>
2019-03-13 11:46:33 +0100

    server updated!

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,32 @@ require('dotenv').config()
 import dbSetup from './db'
 import facebook from './fetch/facebook'
 import google from './fetch/google'
-import { Platform } from './Campaign/model';
+import { Platform } from './Campaign/model'
+import { GraphQLServer } from 'graphql-yoga'
+import { Query, Campaign, Mutation, CampaignPerformance } from './resolvers'
+
+
+const resolvers = {
+  Query,
+  Campaign,
+  Mutation,
+  CampaignPerformance
+}
+
+
+
+
+
 
 dbSetup().then(() => {
   // new facebook(Platform.FACEBOOK).start()
-  new google(Platform.GOOGLE).start()
+
+  const server = new GraphQLServer({ 
+    typeDefs: './src/schema.graphql', 
+    resolvers
+  })
+
+  server.start(() => console.log('Server is running on localhost:4000'))
 })
  
 
